ec123ef0959b11ab882f47e51ab27bc64ef8e48e
kugimiya <bp.001@yandex.ru>
2024-05-31 21:45:20 +0400

    feature: add injecting extra functionality

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,52 @@
-import { Worker } from 'node:worker_threads';
+import { Worker } from "node:worker_threads";
 import { EventEmitter } from "node:events";
-import { readFileSync } from 'node:fs';
+import { readFileSync } from "node:fs";
 
 enum ToWorkerMessageType {
-  DoWork
+  DoWork,
 }
 
 type ToWorkerMessage<WorkerPayload> = {
-  type: ToWorkerMessageType,
+  type: ToWorkerMessageType;
   payload: {
-    task_id: number,
-    task_data: WorkerPayload,
-  }
+    task_id: number;
+    task_data: WorkerPayload;
+  };
 };
 
 enum ToParentMessageType {
   WorkerPort,
-  WorkerResult
+  WorkerResult,
 }
 
-type ToParentMessage<WorkerResult> = {
-  type: ToParentMessageType.WorkerPort,
-  payload: MessagePort,
-} | {
-  type: ToParentMessageType.WorkerResult,
-  payload: {
-    task_id: number,
-    result: WorkerResult
-  },
-};
+type ToParentMessage<WorkerResult> =
+  | {
+      type: ToParentMessageType.WorkerPort;
+      payload: MessagePort;
+    }
+  | {
+      type: ToParentMessageType.WorkerResult;
+      payload: {
+        task_id: number;
+        result: WorkerResult;
+      };
+    };
 
 function worker_logic<WorkerPayload>() {
-  'use main';
+  "use deps";
+  "use main";
 
-  const { parentPort } = require('node:worker_threads');
+  const { parentPort } = require("node:worker_threads");
 
-  parentPort.on('message', (message: ToWorkerMessage<WorkerPayload>) => {
+  parentPort.on("message", (message: ToWorkerMessage<WorkerPayload>) => {
     if (message.type === ToWorkerMessageType.DoWork) {
       // @ts-ignore cause main will be defined in 'use main'; section at runtime
-      (main as (...args: any) => Promise<any>)(message.payload.task_data)
-        .then(result => parentPort?.postMessage({
+      (main as (...args: any) => Promise<any>)(message.payload.task_data).then((result) =>
+        parentPort?.postMessage({
           type: ToParentMessageType.WorkerResult,
-          payload: { task_id: message.payload.task_id, result }
-        }));
+          payload: { task_id: message.payload.task_id, result },
+        }),
+      );
     }
   });
 
@@ -59,17 +63,21 @@ export class ETP<WorkerPayload, WorkerResult> {
   task_events: EventEmitter = new EventEmitter();
   tasks: Record<number, (worker: Worker) => void> = {};
 
-  constructor(protected count: number, protected worker_main: (data: WorkerPayload) => Promise<WorkerResult>) {
-    if (this.worker_main.name !== 'main') {
+  constructor(
+    protected count: number,
+    protected worker_main: (data: WorkerPayload) => Promise<WorkerResult>,
+    protected additional_deps = "",
+  ) {
+    if (this.worker_main.name !== "main") {
       throw new Error('worker_main must be a named async function with name="main"');
     }
 
     // setup task loopin'
-    this.task_events.on('task_complete', (task_id: number) => {
+    this.task_events.on("task_complete", (task_id: number) => {
       this.try_to_emit_next_task();
     });
 
-    this.task_events.on('task_added', (task_id: number) => {
+    this.task_events.on("task_added", (task_id: number) => {
       this.try_to_emit_next_task();
     });
   }
@@ -84,13 +92,16 @@ export class ETP<WorkerPayload, WorkerResult> {
     // setup workers
     for (let i = 0; i < this.count; i++) {
       const main_as_text = this.worker_main.toString();
-      const worker_code = readFileSync(__filename).toString()
-        .replace(`'use` + ` main';`, main_as_text) + `\n worker_logic();`;
+      const worker_code =
+        readFileSync(__filename)
+          .toString()
+          .replace(`"use` + ` main";`, main_as_text)
+          .replace(`"use` + ` deps";`, this.additional_deps) + `\n worker_logic();`;
 
       const thread = new Worker(worker_code, { eval: true });
       this.threads.push(thread);
 
-      thread.on('message', (message: ToParentMessage<WorkerResult>) => {
+      thread.on("message", (message: ToParentMessage<WorkerResult>) => {
         if (message.type === ToParentMessageType.WorkerPort) {
           received_ports_count += 1;
 
@@ -106,7 +117,7 @@ export class ETP<WorkerPayload, WorkerResult> {
             this.threads_promise_resolvers[task_id](result);
             delete this.threads_promise_resolvers[task_id];
           }
-          this.task_events.emit('task_complete', task_id);
+          this.task_events.emit("task_complete", task_id);
         }
       });
     }
@@ -120,17 +131,17 @@ export class ETP<WorkerPayload, WorkerResult> {
     return new Promise((resolve) => {
       this.threads_promise_resolvers[task_id] = resolve;
       this.tasks[task_id] = (worker: Worker) => {
-        worker.postMessage({ type: ToWorkerMessageType.DoWork, payload: { task_id, task_data } })
+        worker.postMessage({ type: ToWorkerMessageType.DoWork, payload: { task_id, task_data } });
       };
-      this.task_events.emit('task_added', task_id);
+      this.task_events.emit("task_added", task_id);
     });
   }
 
   try_to_emit_next_task() {
     const unavailable_threads = Object.values(this.threads_to_task_id_map);
     const available_threads = Object.keys(this.threads)
-      .map(_ => Number(_))
-      .filter(_ => !unavailable_threads.includes(_));
+      .map((_) => Number(_))
+      .filter((_) => !unavailable_threads.includes(_));
 
     if (available_threads.length > 0 && Object.keys(this.tasks).length > 0) {
       const thread_index = available_threads.pop();
